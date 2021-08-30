1b965284f7407c6c221b23122874c01696ea1877
Varun Divi <vdivi33@ad.unc.edul>
2021-08-30 14:04:38 -0400

    save

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,10 @@
+"""Demonstartion of numeric operators"""
+__author__ = "730470448"
+left_side: str = input("Left-hand side: ")
+right_side: str = input("Right-hand side: ")
+left: int = int(left_side)
+right: int = int(right_side)
+print(left_side + " ** " + right_side + " is " + str(left**right))
+print(left_side + " / " + right_side + " is " + str( left / right ))
+print(left_side + " // " + right_side + " is " + str( left // right ))
+print(left_side + " % " + right_side + " is " + str( left % right ))
