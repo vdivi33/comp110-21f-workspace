c1594fc633e515d9ff763c77b458aa7f387eb8df
Varun Divi <vdivi33@ad.unc.edul>
2021-08-30 16:31:02 -0400

    save2

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,9 +1,9 @@
 # TODO: Write docstring here
-"""A Machine To Hype you up"""
+"""A Machine To Hype you up!"""
 # TODO: Initialize __author__ variable here
 __author__ = "730470448"
 # TODO: Implement your program logic here
 name: str = input("What is your name? ")
 print(name + " Keep your chin up!")
 print("You're gonna do great today " + name)
-print("You're almost there " + name + " keep it up!!") 
+print("You're almost there " + name + " keep it up!!")
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,41 +1,9 @@
-"""Relational operator demonstration"""
+"""Relational operator demonstration!"""
 __author__ = "730470448"
 
 left_hand: str = input("Left-hand side: ")
 right_hand: str = input("Right-hand side: ")
-
-if(left_hand < right_hand):
-    {
-        print(left_hand +" < " + right_hand + " is True")
-    }
-else: 
-    print(left_hand + " < " + right_hand + " is False")
-
-
-
-if(left_hand >= right_hand):
-    {
-        print(left_hand + " >= " + right_hand + " is True")
-    }
-else:
-    print(left_hand + " >= " + right_hand + " is False")
-
-
-
-if(left_hand == right_hand):
-    {
-        print(left_hand + " == " + right_hand+ " is True")
-    }
-else:
-    print(left_hand + " == " + right_hand + " is False")
-
-
-
-if(left_hand != right_hand):
-    {
-        print(left_hand + " != " + right_hand + " is True")
-    }
-else:
-    print(left_hand + " != " + right_hand + " is False")
-
-
+print(left_hand + " < " + right_hand + " is " + str(left_hand < right_hand))
+print(left_hand + " >= " + right_hand + " is " + str(left_hand >= right_hand))
+print(left_hand + " == " + right_hand + " is " + str(left_hand == right_hand))
+print(left_hand + " != " + right_hand + " is " + str(left_hand != right_hand))
