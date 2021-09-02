9fe7007cd9a8044511b735fa594c52c44cfd0534
Varun Divi <vdivi33@ad.unc.edul>
2021-09-02 15:52:09 -0400

    ex02

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,16 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730470448"
 
 
 # Begin your solution here...
+letter: str = input("What letter do you want to search for?: ")
+word: str = input("Enter a word: ")
+count: int = 0
+length: int = len(word)
+i: int = 1
+while length > 0:
+    if(word[length - 1] == letter):
+        count = count + 1
+    length = length - 1
+print("Count: " + str(count))
--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730470448"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,15 @@ from random import randint
 
 
 # Begin your solution here...
+print("Your fortune cookie says...")
+random_int = randint(1, 4)
+print(random_int)
+if(random_int == 1):
+    print("You will find exactly what you are looking for!")   
+elif(random_int == 2):
+    print("Fortune and sucess lies ahead!")
+elif(random_int == 3):
+    print("You will soon make lifelong friends")
+elif(random_int == 4):
+    print("Keep your eye out for someone special!")
+print("Now, go spread positive vibes!")
