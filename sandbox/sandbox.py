9a1ac7228282b21e73f0aaea270631c5056b53b9
Varun Divi <vdivi33@ad.unc.edul>
2021-09-16 18:02:22 -0400

    sandbox

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,12 +1,8 @@
 i: int = 0
-s: str = ""
-
-while i < 4:
-    if i % 2 == 0:
-        s = s + "h"
-    else:
-        s = s + "e"
-    i = i + 1
-print(True and False and False or not False)
-print(s)
-print(i)
+j: int = 9
+while i < 1:
+    print ("ji")
+    while j > 0:
+        print("hi")
+        i = 9
+    i = 9
