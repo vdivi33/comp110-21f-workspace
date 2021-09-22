10bfb465aae0120eb9928a96bc38086afe44a640
Varun Divi <vdivi33@ad.unc.edul>
2021-09-22 10:51:29 -0400

    c

---
+++ projects/Coin.py
@@ -0,0 +1,22 @@
+
+print("  || ")
+print(" |||| ")
+print(" |||| ")
+print("  || ")
+
+
+print(" _____")
+print("_     _")
+print("_     _")
+print(" _____")
+
+print("  || ")
+print(" |||| ")
+print(" |||| ")
+print("  || ")
+
+print(" _____")
+print("_     _")
+print("_  H  _")
+print(" _____")
+
