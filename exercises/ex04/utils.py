22cf233cff011a2292f2f69f11c2527e30605427
Varun Divi <vdivi33@ad.unc.edul>
2021-09-26 17:39:22 -0400

    c

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -2,5 +2,20 @@
 
 __author__ = "123456789"
 
-
+l = [1,2,3,4]
+l2 = [1,1,1,1]
 # TODO: Implement your functions here.
+
+
+def all(list, match: int) -> bool:
+    int i = 0
+    int count = 0
+    while i < len(list):
+        if(list[i] == match):
+            count+=count
+    if(count == len(list)):
+        return True
+    return False
+
+print(all(l, 1))
+print(all(l2, 1))
