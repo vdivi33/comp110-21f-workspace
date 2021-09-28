a5b653fc4491a7624ea83b1cfe2e300d82fd77c3
Varun Divi <vdivi33@ad.unc.edul>
2021-09-28 17:48:02 -0400

    c

--- lessons/sum.py
+++ lessons/sum.py
@@ -3,7 +3,9 @@
 
 def sum(xs: list[float]) -> float:
     """Compute the sum of a list."""
-    if( len(xs) == 0.0):
-        return 0.0
-    else:
-        return xs[0]
+    total: float = 0.0
+    i: int = 0
+    while i < len(xs):
+        total += xs[i]
+        i += 1
+    return total
