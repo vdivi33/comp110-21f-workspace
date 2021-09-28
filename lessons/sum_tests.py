4a586d27c33cddb35055471705baaa427a1ad690
Varun Divi <vdivi33@ad.unc.edul>
2021-09-28 17:38:42 -0400

    c

--- lessons/sum_tests.py
+++ lessons/sum_tests.py
@@ -8,6 +8,7 @@ def test_sum_empty() -> None:
     xs: list[float] = []
     assert sum(xs) == 0.0
 
+
 def test_sum_single_item() -> None:
     xs: list[float] = [110.0]
     assert sum(xs) == 110.0 
