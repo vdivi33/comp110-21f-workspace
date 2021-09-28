012ad08e180cb447c38ca91b14000094c95292df
Varun Divi <vdivi33@ad.unc.edul>
2021-09-28 17:33:48 -0400

    c

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -5,7 +5,8 @@ __author__ = "123456789"
 # TODO: Implement your functions here.
 
 
-def all(list, match: int) -> bool:
+def all(list: list[int], match: int) -> bool:
+    """Finds an integer in a list that matches a desired number. Returns True if found and returns False otherwise."""
     i: int = 0
     count: int = 0
     if(len(list) > 0):
@@ -18,22 +19,31 @@ def all(list, match: int) -> bool:
     return False
 
 
-def is_equal(list_one: list[int], list_two: list[int]):
+def is_equal(list_one: list[int], list_two: list[int]) -> bool:
+    """Compares the elements of two lists and returns True if all the elements are the same."""
     i: int = 0
     j: int = 0
     count: int = 0
-    while(i < len(list_one)):
-        while(j < len(list_two)):
+    if(len(list_one) < len(list_two)):
+        while(i < len(list_one)):
             if(list_one[i] == list_two[j]):
                 count = count + 1
             j = j + 1
-        i = i + 1
+            i = i + 1
+    else:
+        while(i < len(list_two)):
+            if(list_one[i] == list_two[j]):
+                count = count + 1
+            j = j + 1
+            i = i + 1
+    print(count)
     if(count == len(list_one) and count == len(list_two)):
         return True
     return False
 
 
 def max(input: list[int]) -> int:
+    """Finds and returns the greatest integer in a list."""
     i: int = 1
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
@@ -44,3 +54,5 @@ def max(input: list[int]) -> int:
                 max = input[i]
             i = i + 1
     return max
+
+# print(is_equal([1,2,1,3,],[1,1,2,3]))
---
+++ lessons/sum.py
@@ -0,0 +1,6 @@
+"""Example of writing a Test subject."""
+
+
+def sum(xs: list[float]) -> float:
+    """Compute the sum of a list."""
+    return 0.0
