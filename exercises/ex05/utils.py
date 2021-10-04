4e102b3b419b2254dff383dd39cba15c77ecf5f5
Varun Divi <vdivi33@ad.unc.edul>
2021-10-04 14:02:22 -0400

    c

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,30 +4,27 @@ __author__ = "730470448"
 
 # Define your functions below
 
-def only_evens(l: list[int]) -> list[int]:
+
+def only_evens(lis: list[int]) -> list[int]:
     l2: list[int] = []
-    for i in l:
+    for i in lis:
         if(i % 2 == 0):
             l2.append(i)
     return l2
         
-print(only_evens([444,44,4]))
 
-
-def sub(l: list[int], start: int, end: int):
+def sub(lis: list[int], start: int, end: int):
     l2: list[int] = []
     i: int = start
     while i < end:
-        l2.append(l[i])
+        l2.append(lis[i])
         i = i + 1
     return l2
-print(sub([10,20,30,40], 1, 3))
 
-def concat(l: list[int], l2: list[int]):
+
+def concat(lis: list[int], l2: list[int]):
     i: int = 0
     while i < len(l2):
-        l.append(l2[i])
+        lis.append(l2[i])
         i = i + 1
-    return l
-
-print(concat([1, 2, 3, 4],[6, 7, 8, 9]))
+    return lis
