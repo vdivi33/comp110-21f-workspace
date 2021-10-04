4341361a9ab77f9abbf7ce24a42e48aa5cfe8d2b
Varun Divi <vdivi33@ad.unc.edul>
2021-10-04 12:29:12 -0400

    c

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,33 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730470448"
 
 # Define your functions below
+
+def only_evens(l: list[int]) -> list[int]:
+    l2: list[int] = []
+    for i in l:
+        if(i % 2 == 0):
+            l2.append(i)
+    return l2
+        
+print(only_evens([444,44,4]))
+
+
+def sub(l: list[int], start: int, end: int):
+    l2: list[int] = []
+    i: int = start
+    while i < end:
+        l2.append(l[i])
+        i = i + 1
+    return l2
+print(sub([10,20,30,40], 1, 3))
+
+def concat(l: list[int], l2: list[int]):
+    i: int = 0
+    while i < len(l2):
+        l.append(l2[i])
+        i = i + 1
+    return l
+
+print(concat([1, 2, 3, 4],[6, 7, 8, 9]))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,21 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730470448"
+
+
+def only_evens_test_onlyOdds() -> None:
+    assert only_evens([1,3,5,7]) == []
+
+def only_evens_test_empty() -> None:
+    assert only_evens([]) == []
+
+def only_evens_test_normal() -> None:
+    assert only_evens([1,2,3,4,5,6,7,8]) == [2,4,6,8]
+
+
+def only_evens_test_normal2() -> None:
+    assert only_evens([1,6,4,8]) == [6,4,8]
+
