fc869cf77026f42b20fb367c9e1f8cff616e67c8
Varun Divi <vdivi33@ad.unc.edul>
2021-10-09 18:07:02 -0400

    c

--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,24 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
 __author__ = "730470448"
+
+def test_base_case_invert() -> None:
+    assert invert({'a' : 'A', 'b' : 'B'}) == {'A' : 'a', 'B' : 'b'}
+
+def test_edge_case_invert() -> None:
+    assert invert({'a' : 'a', 'b' : 'b'}) == {'a' : 'a', 'b' : 'b'}
+
+def test_base_case_invert2() -> None:
+    assert invert({'a' : 'b', 'c' : 'd', 'e' : 'f'}) == {'b' : 'a', 'd' : 'c', 'f' : 'e'}
+
+def test_base_case_favorite_color() -> None:
+    assert favorite_color({"Marc": "yellow","Jane" : "yellow","Bob":"yellow", "John" : "green", "Tom": "green", "Ezri": "blue", "Kris": "blue","Gary":"blue"}) == 'yellow'
+
+def test_base_case_favorite_color2() -> None:
+    assert favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}) == "blue"
+
+def test_edge_case_favorite_color() -> None:
+    assert favorite_color("Marc":"yellow") == "yellow"
