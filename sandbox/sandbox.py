d42da6ef4f8379e8a11327c2631d10c77cd8f6d4
Varun Divi <vdivi33@ad.unc.edul>
2021-10-29 13:33:30 -0400

    c

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,8 +1,16 @@
-i: int = 0
-j: int = 9
-while i < 1:
-    print ("ji")
-    while j > 0:
-        print("hi")
-        i = 9
-    i = 9
+def spooky()-> None:
+    b: dict[int, int] = {0: 1}
+    o: dict [int, int] = b
+
+    b = sz(b)
+
+    print(b[0] + o[0])
+
+def sz(n: dict [int, int]) -> dict[int, int]:
+    b: dict[int, int] = n
+    print(b)
+    n = {0: 3}
+    b[0] = 2
+    return n
+
+spooky()
