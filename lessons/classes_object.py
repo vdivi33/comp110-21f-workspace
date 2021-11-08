0f2ad5103c3bcbdbe0a2e29a25ff127a2977a99f
Varun Divi <vdivi33@ad.unc.edul>
2021-11-07 22:47:28 -0500

    c

---
+++ lessons/classes_object.py
@@ -0,0 +1,43 @@
+"""example of a class and object instantiation."""
+
+
+class Pizza:
+    """Models the idea of a Pizza."""
+    size: str
+    toppings: int
+    extra_cheese: bool = False
+
+
+    def __init__(self, size: str, toppings: int):
+        self.size = size
+        self.toppings = toppings
+
+
+    def price(self, tax: float) -> float:
+        """Calculate the price of a pizza."""
+        total: float = 0.0
+        if self.size == "large":
+            total += 10.0
+        else:
+            total += 8.0
+        
+        total += self.toppings * 0.75
+
+        if self.extra_cheese == True:
+            total += 1.0
+
+        total += total * tax
+
+        return total
+
+
+a_pizza: Pizza = Pizza("large", 3)
+print(a_pizza)
+print(a_pizza.size)
+# Method call example
+print(f"Price: ${a_pizza.price(1.05)}")
+
+another_pizza: Pizza = Pizza("small", 0)
+another_pizza.extra_cheese = True
+print(another_pizza.size)
+print(f"Price: ${another_pizza.price(1.05)}")
