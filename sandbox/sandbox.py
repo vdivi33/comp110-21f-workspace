e0f34a22f55c7c53ca16496434ac54c428be0723
Varun Divi <vdivi33@ad.unc.edul>
2021-11-29 18:07:29 -0500

    f f

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,16 +1,4 @@
-def spooky()-> None:
-    b: dict[int, int] = {0: 1}
-    o: dict [int, int] = b
+import numpy
 
-    b = sz(b)
-
-    print(b[0] + o[0])
-
-def sz(n: dict [int, int]) -> dict[int, int]:
-    b: dict[int, int] = n
-    print(b)
-    n = {0: 3}
-    b[0] = 2
-    return n
-
-spooky()
+x = np.arange(1, 5)
+print(x)
